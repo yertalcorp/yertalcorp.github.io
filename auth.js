2f013b7026c30b90ec9318d9cfe2fd782da11374
yertalcorp <yertalcorp@yahoo.com>
2026-02-03 14:41:24 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,21 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { 
-    getAuth, 
-    signInWithEmailAndPassword, 
-    signOut, 
-    onAuthStateChanged 
-} from "firebase/auth";
-
-// TASK: Import the centralized config from your specific config file
-import { firebaseConfig } from './firebase-config.js';
-
-// Initialize Firebase App using the imported config
-const app = initializeApp(firebaseConfig);
-
-// Initialize and Export Auth Instance
-export const auth = getAuth(app);
-
-/* OBJECTIVE: Exported Auth Utilities */
+import { auth } from './firebase-config.js';
+import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 // Login function
 export const login = (email, password) => {
