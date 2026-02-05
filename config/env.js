034993183402ba8e4087110c396f607648422bb6
YERTAL CORP <yertalcorp@yahoo.com>
2026-02-05 13:59:43 +0530

    Update and rename env.js to env.js

---
+++ config/env.js
@@ -0,0 +1,5 @@
+JavaScript
+// config/env.js
+export const ENV = {
+    GEMINI_KEY: "AIzaSyCQXYKsnXL7cEftmeKbVWWUYGckZvV4YDo"
+};
--- js/env.js
+++
@@ -1,5 +0,0 @@
-JavaScript
-// config/env.js
-export const ENV = {
-    GEMINI_KEY: "AIzaSyBm_hgTArPco_CCk__xPadS39vK6eJSAMs"
-};
