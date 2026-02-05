23fb037b8f945cf5db9f8390c7b885a1fdd630ef
YERTAL CORP <yertalcorp@yahoo.com>
2026-02-05 21:24:13 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,8 @@ import {
     signInWithPopup,
     signInWithRedirect,
     GoogleAuthProvider, 
-    GithubAuthProvider, 
+    GithubAuthProvider,
+    OAuthProvider,
     signOut, 
     onAuthStateChanged 
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
