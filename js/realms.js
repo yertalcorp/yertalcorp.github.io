39ccc27b6c032975fd73dda50bfbe67b4b449616
YERTAL CORP <yertalcorp@yahoo.com>
2026-07-04 13:39:38 +0530

    Update realms.js

--- js/realms.js
+++ js/realms.js
@@ -549,7 +549,7 @@ watchAuthState(async (newUser) => {
         }
     }
 });
-aysnc function (hero) {
+aysnc function renderHero (hero) {
     const el = document.getElementById('hero-container');
     if (!el) return;
     // Calculate action based on the global user variable
