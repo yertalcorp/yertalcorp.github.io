83dc6a0ba26e2a646d8caec0b383b9e51f71d168
YERTAL CORP <yertalcorp@yahoo.com>
2026-08-24 21:40:26 +0530

    Update realm-engine.js

--- js/realm-engine.js
+++ js/realm-engine.js
@@ -9,7 +9,7 @@ window.update = update;
 window.get = get;
 
 // Build Check: Manually update the time string below when pushing new code
-console.log(`%c YERTAL REALM LOADED | ${new Date().toLocaleDateString()} @21:24:00 `, "background: var(--bg-color); color: var(--branding-color); font-weight: bold; border: 1px solid var(--branding-color); padding: 4px;");
+console.log(`%c YERTAL REALM LOADED | ${new Date().toLocaleDateString()} @21:40:00 `, "background: var(--bg-color); color: var(--branding-color); font-weight: bold; border: 1px solid var(--branding-color); padding: 4px;");
 
 /* export variables that spark.js will use */
 export let databaseCache = {};
@@ -643,7 +643,7 @@ const updateSparkViews = async function(realmId, currentId, sparkId, country = '
     }
 }
 // Export for module consumers (like spark.js) for in spark interaction zone
-export {likeSpark, openFeedback,shareSpark,cloneSpark,updateSparkViews, deleteSpark, formatTimeAgo, getUserCountry, payOwner};
+export {likeSpark, openFeedback,shareSpark,cloneSpark,updateSparkViews, deleteSpark, getUserCountry, payOwner};
 
 // Bind to window for inline HTML event handlers
 window.likeSpark = likeSpark;
